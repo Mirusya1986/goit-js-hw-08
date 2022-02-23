1c39531ae1c282187683baa717ad7c453b3e63d9
Mirusya1986 <k.mirochka@gmail.com>
2022-02-24 01:38:35 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,22 @@
 import Player from '@vimeo/player';
+import lodash from 'lodash.throttle';
+
+const STORAGE_KEY = "videoplayer-current-time";
 
 const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('timeupdate', lodash(onPlay,1000)); 
+
+function onPlay({seconds}){
+    localStorage.setItem(STORAGE_KEY, seconds)
+}
+
+setCurrentTime()
+
+function setCurrentTime(){
+    if(!localStorage.getItem(STORAGE_KEY)){
+        return
+    }
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY))
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,9 @@
+import lodash from 'lodash.throttle';
+
+ const refs = {
+     form:document.querySelector(".feedback-form"),
+     email:document.querySelector(".feedback-form input"),
+     textarea:document.querySelector(".feedback-form textarea")
+ }
+const FEEDBACK_STATE = "feedback-form-state";
 
